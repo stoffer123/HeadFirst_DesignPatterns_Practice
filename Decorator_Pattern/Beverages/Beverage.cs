f58f9c321cd3ef9a351fd51cb98e86a52beaff52
christopher mikkelsen <christophermikkelsen@live.dk>
2024-12-12 16:02:15 +0100

    Decorator_Pattern initial

---
+++ Decorator_Pattern/Beverages/Beverage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator_Pattern.Beverages
+{
+    abstract class Beverage
+    {
+        private double cost;
+        private string description;
+
+        public Beverage()
+        {
+            cost = 0.0;
+            description = "Beverage";
+        }
+    }
+}
---
+++ Decorator_Pattern/Beverages/HouseBlend.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Decorator_Pattern.Beverages
+{
+    class HouseBlend : Beverage
+    {
+        private double cost;
+        private string description;
+
+        public HouseBlend()
+        {
+            description = "HouseBlend";
+            cost = 2;
+        }
+
+        
+
+
+    }
+}
